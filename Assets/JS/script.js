ecaf4a9dbb33ae1caa87018259475025a0761c9e
LMSLTOPDOCS <laurabora118@yahoo.com>
2022-05-30 14:48:04 -0700

    updates to variables

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,20 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialChar = "!,@,#,$,%,^,&,*<(,),<,>,?,/,:)";
-var numbers = "0,1,2,3,4,5,6,7,8,9";
-var upperLetters = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-var lowerLetters = "a, b, c, d, e, f, g, h, i , j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
-var randomWords = "Monkey, Peacock, Unicorn, Michigan, Missouri, Arizona, Sunrise, Sunset, Funny, Smile, DavidF, JoeyB";
+var specialChar = ["!", "@", "#", "$" , "%", "^" , "&", "*", "<", "(", ")", ",", "<", ">", "?", "/", ":"];
+var numbers = ["0", "1", "2", "3" , "4", "5", "6", "7", "8", "9"];
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var randomWords = ["Monkey", "Peacock", "Unicorn", "Michigan", "Missouri", "Arizona", "Sunrise", "Sunset", "Funny", "Smile", "DavidF", "JoeyB", "Hehe"];
 
-function generatePassword(){
-  var generatePassword = "generatedpassword";
-  console.log("clicket generate password");
-  return generatedPassword:
+function makePassword(){
+  var makePassword = "makepassword";
+  console.log("click make password");
+  return makePassword
   }
   
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = makePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
