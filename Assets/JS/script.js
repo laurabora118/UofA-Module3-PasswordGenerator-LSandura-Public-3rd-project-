1e22ae4e03430a9ba952f268f3882b7d0cab682b
LMSLTOPDOCS <laurabora118@yahoo.com>
2022-05-31 04:57:23 -0700

    java updates

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,7 +2,6 @@
 // var minlength ()
 // var maxlength ()
 //after getting answers do some looping
-
 var generateBtn = document.querySelector("#generate");
 
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -12,7 +11,7 @@ var specialChar = ["!", "@", "#", "$" , "%", "^" , "&", "*", "<", "(", ")", ",",
 var miscWords = ["monkey", "peacock", "unicorn", "michigan", "missouri", "arizona", "sunrise", "sunset", "smile", "davidf", "joeyb", "funny", "hehe", "movies", "brick", "purple", "pink", "black", "singing", "tophits", "bootcamp", "developer", "income", "opportunity", "decision", "pizza"];
 
 function generatePassword() {
-  var generatedPassword = "";
+  var generatedPassword = "generated password";
   console.log("clicked generate password");
   return generatedPassword;
   
@@ -20,13 +19,6 @@ function generatePassword() {
 // if (promptUpper = yes) 
   //("Your password is: " + newPassword); 
   //console.log("Your password is: " + upperLetters[i]+numbers[i]+lowerLetters[i]+specialChar[i]+miscWords[i]);
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
 }
 
 for (var clickButton of
@@ -38,54 +30,60 @@ for (var clickButton of
   }
 
   var options = ["Yes", "No"];
-  var gatheroptions = function (){
+  var gatheroptions = function () {
     var promptUpper = window.prompt("Type Yes if you want upper case letters, type No if you do not.");
     var promptNumb = window.prompt("Type Yes if you want numbers, type No if you do not.");
     var promptLCase = window.prompt("Type Yes if you want lower case letters, type No if you do not.");
     var promptSpecial = window.prompt("Type Yes if you want special characters, type No if you do not.");
-    var promptRandome = window.prompt("Type Yes if you want random words, type No if you do not.");
-    setminmax();
-      }
-    
+    var promptRandom = window.prompt("Type Yes if you want random words, type No if you do not.");
+    if (!options === "No") { 
+      window.alert ("You need to select yes to at least one yes option. Start over.");
+      setminmax ();
+  
   var setminmax = function () {
     var promptMinlength = window.prompt("What is your minimum length? Pick a number at 8 or above but less than 128")
-    var promptMaxlength = window.prompt("What is your maximum length? Pick a number at 128 or below but more than 8")
-    
-  function getRandomIntInclusive(min,max) {
+    var promptMaxlength = window.prompt("What is your maximum length? Pick a number at 128 or below but more than 8")}
+    if (promptMinlength < 8) {
+      window.alert ("You need to select options above 8 - less than 128, start over");
+    }  else if 
+      (promptMaxlength > 128) {
+      window.alert ("You need to select below 128 - more than 8, start over");
+    }
+  } 
+  function setlength (min,max) {
     min = Math.ceil(promptMinlength)
     max = Math.floor(promptMaxlength)
     return "length"
   }
-  var passgenerator = [upperLetters, numbers, lowerLetters, specialChar, miscWords]
-  let arr = [0,1,2,3,4,5]
-  for (let i = 0; i <arr.length; i++)
-    console.log(arr[i]);
-  }
-
-  generateBtn.addEventListener("click", writePassword);
-
-//var proceed = confirm("Are you sure you want to proceed?");
-//if (proceed) {
-//  } else {
-//  }
+}
     
-//  document.getElementsByClassName("alert"))
-//  var askQuestions = function() {
-//     prompt("Press Okay if you want upper case letters");
-//     prompt("Press Okay if you want numbers");   
-//  }
-     
-//function getRandomIntInclusive(min, max) {
-//  min = Math.ceil(min);
-//  max = Math.floor(max);
-//  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-//}
-  // If user pressed Cancel, immediately end function
+//  if (promptMinlength <=8 & promptMaxlength >= 128)
+  var Array = [upperLetters+numbers+lowerLetters+specialChar+miscWords]
+  //finalArray[Math.floor(Math.random()*arr.length)]
+  //var passgenerator = [upperLetters, numbers, lowerLetters, specialChar, miscWords]
+  //var array1 = [Math.floor(Math.random(upperLetters,numbers,lowerLetters,specialChar,miscWords))*]
+  for(var i=0; i < Array; i++); {
+    // This statement will run each time the loop is executed
+    console.log("Here is your password: " + Array[i++]);
+  }
+ 
+  //for (let i = 0; i <arr.length; i++)
+  //  console.log(arr[i]);
+  
+    // Write password to the #password input
 
+  function writePassword() {
+  var password = generatePassword(Array);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+//(promptUpper = "No" && promptNumb = "No" && promptLCase = "No" && promptSpecial = "No" && promptRandom = "No")
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 //  if (randomPassword.isgenerated) {
 //    console.log("Password is: " + generatedPassword);
 //  }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
